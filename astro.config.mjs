5f82754dba00905ebc8aef73809ef7bb7029c6d9
RayhanGMAN <rayhangaruthman@gmail.com>
2026-07-10 17:48:05 -0700

    modified astro.mjs file

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://github.com/RayhanGMAN/devportfolio.git",
+  base: "/devportfolio/",
   vite: {
     plugins: [tailwindcss()],
   },
